4f0746fa20f0f8123c5eaeb7020b06d4e8610e4a
karimatouhami <karimatouhami246@gmail.com>
2023-04-28 06:07:47 -0700

    a

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,8 @@ void print_number(int n)
 	{
 		n1 = -n;
 		_putchar('-');
-	} else
+	}
+	else
 	{
 		n1 = n;
 	}
@@ -23,11 +24,4 @@ void print_number(int n)
 	}
 
 	_putchar((n1 % 10) + '0');
-}
-
-int main(void)
-{
-	print_number(98);
-	_putchar('\n');
-	return (0);
 }
