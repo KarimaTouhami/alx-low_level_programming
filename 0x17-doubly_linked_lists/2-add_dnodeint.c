0d3ba6388ec7d7fcc14c0ca0dfe9ec79de8eb686
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-12-07 17:58:36 +0000

    Insert at index

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,17 +10,18 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(*new_node));
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-    if (head && new_node)
-    {
-        new_node->n = n;
-        new_node->prev = NULL;
-        new_node->next = *head;
-        if (*head)
-            (*head)->prev = new_node;
-        *head = new_node;
-        return (new_node);
-    }
-    return (NULL);
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+	return (new_node);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,24 +8,29 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(*new_node));
-    dlistint_t *last_node = *head;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current = *head;
 
-    if (head && new_node)
-    {
-        new_node->n = n;
-        new_node->next = NULL;
-        if (!*head)
-        {
-            new_node->prev = NULL;
-            *head = new_node;
-            return (new_node);
-        }
-        while (last_node->next)
-            last_node = last_node->next;
-        last_node->next = new_node;
-        new_node->prev = last_node;
-        return (new_node);
-    }
-    return (NULL);
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (current == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+
+	else
+	{
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = new_node;
+		new_node->prev = current;
+	}
+
+	return (new_node);
 }
